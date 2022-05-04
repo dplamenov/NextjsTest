893916f29b9cdaa385f93dcbc66d4567984488da
dplamenov <dimitar.plamenov@gmail.com>
2022-05-04 21:50:35 +0300

    Update ManagedAgency.jsx

--- sections/ManagedAgency/ManagedAgency.jsx
+++ sections/ManagedAgency/ManagedAgency.jsx
@@ -1,7 +1,10 @@
 import {
   Heading,
   StyledContainer,
-  Subtitle
+  Subtitle,
+  RightContainer,
+  Flex,
+  Element
 } from "./elements";
 import Image from "next/image";
 
@@ -11,9 +14,31 @@ export const ManagedAgency = () => {
       <StyledContainer style={{ maxWidth: '50%' }}>
         <Heading>Managed agency selection</Heading>
         <Subtitle>Stengthen your onboarding process</Subtitle>
-        <Image src="/img/video.png" width="400px" height="600px" alt="image" />
+        <Flex>
+          <Image src="/img/video.png" width="400px" height="600px" alt="image" />
+          <RightContainer>
+            <Element>
+              <h4>Brief</h4>
+              <p style={{textAlign: 'left'}}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sapiente.
+              </p>
+            </Element>
+            <Element>
+              <h4>Search</h4>
+              <p style={{ textAlign: 'left' }}>
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sapiente.
+              </p>
+            </Element>
+            <Element>
+              <h4>Pitch</h4>
+              <p style={{ textAlign: 'left' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, sapiente.
+              </p>
+            </Element>
+          </RightContainer>
+        </Flex>
       </StyledContainer>
-      
     </StyledContainer>
   );
 };
