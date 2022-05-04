fcfc729ed7c1524a6af795f02792047dea1dffbe
dplamenov <dimitar.plamenov@gmail.com>
2022-05-04 21:21:14 +0300

    create ManagedAgency section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { Hero } from "../sections";
+import { Hero, ManagedAgency } from "../sections";
+
 
 const heroProps = {
   image: { src: "/img/heroimg.png", alt: "", width: 1000, height: 1680 },
@@ -20,6 +21,7 @@ export default function Home() {
       </head>
       <div>
         <Hero {...heroProps} />
+        <ManagedAgency />
         {/** Other sections */}
       </div>
     </>
