2bbfa6906b2f723303f97c8b0639fd4e1cd179f5
dplamenov <dimitar.plamenov@gmail.com>
2022-05-04 21:50:33 +0300

    Update elements.jsx

--- sections/ManagedAgency/elements.jsx
+++ sections/ManagedAgency/elements.jsx
@@ -11,7 +11,27 @@ export const Heading = styled((props) => <SectionBigHeading {...props}  />)`
 text-align: center;
 `;
 
-export const Subtitle = styled((props) => <SectionSubheading {...props}  />)`
+export const Subtitle = styled((props) => <SectionSubheading {...props} />)`
 text-align: center;
 `;
+
+export const RightContainer = styled((props) => <div {...props}  />)`
+text-align: center;
+margin-left: 50px;
+`;
+
+export const Flex = styled((props) => <div {...props} />)`
+display: flex;
+`;
+
+export const Element = styled((props) => <div {...props} />)`
+background: lightgray;
+height: 150px;
+font-family: sans-serif;
+padding: 5px;
+margin: 10px 0;
+border-radius: 10px;
+text-align: left;
+`;
+
 
